02515a9faea4389a6f2872166a9d6907364be762
BabysrinithiAnanthan <163375957+BabysrinithiAnanthan@users.noreply.github.com>
2026-08-29 15:37:01 +0530

    Enhance order routes with additional functionalities

--- orderRoutes.js
+++ orderRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express'
-import { createOrder, getOrders } from '../Controllers/orderController.js';
+import { createOrder, getAllOrders, getMyOrders,cancelOrder, updateOrder} from '../Controllers/orderController.js';
+import { protect } from '../middleware/authMiddleware.js';
+import {adminOnly} from '../middleware/adminMiddleware.js';
+
+
 
 const router = express.Router();
 
-router.use("/",createOrder);
-router.use("/",getOrders);
+router.post("/",protect,createOrder);
+router.get("/all",protect,adminOnly,getAllOrders);
+router.put("/:id/cancel",protect,cancelOrder);
+router.get("/myOrders",protect,getMyOrders);
+router.put("/:id/status",protect,adminOnly,updateOrder);
 
 export default router;
